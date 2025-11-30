bb3dfb436d9e7965492ac6f4bb25719d611d4771
Mauli Radhika <mauliradhika05@gmail.com>
2025-11-30 15:32:44 +0530

    Add Games

---
+++ Basics/calculator.py
@@ -0,0 +1,19 @@
+print("Welcome to the Calculator")
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed")
+    else:
+        print(num1 / num2)
+else:
+    print("Invalid operator")
---
+++ Basics/mad_libs.py
@@ -0,0 +1,12 @@
+# Simple Mad Libs Game
+
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+place = input("Enter a place: ")
+
+print("\n--- Your Mad Libs Story ---")
+print("Today I went to the " + place + ".")
+print("I saw a really " + adjective + " " + noun + ".")
+print("It suddenly started to " + verb + "!")
+print("It was the funniest thing ever!")
